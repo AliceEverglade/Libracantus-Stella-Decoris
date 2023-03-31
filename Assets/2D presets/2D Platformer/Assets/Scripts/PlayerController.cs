7dd0eded24b6e31ef445d99844d05512513dd6d7
AliceEverglade <91664294+AliceEverglade@users.noreply.github.com>
2023-04-01 01:49:54 +0200

    no idea tbh

--- Assets/2D presets/2D Platformer/Assets/Scripts/PlayerController.cs
+++ Assets/2D presets/2D Platformer/Assets/Scripts/PlayerController.cs
@@ -7,15 +7,36 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
-    [SerializeField] private float movementInputDirection;
+
+    [Header("State Booleans")]
     [SerializeField] private bool isFacingRight = true;
     [SerializeField] private bool isGrounded;
+    [SerializeField] private bool isTouchingWall;
+
+    [Header("Movement")]
+    [SerializeField] private float movementInputDirection;
     [SerializeField] private float movementSpeed;
+
+    [Header("Jumping")]
     [SerializeField] private float jumpForce;
+    [SerializeField] private int jumpCount;
+    [SerializeField] private int currentJumpCount;
+    [SerializeField] private bool canJump;
+
+    [Header("groundCheck")]
     [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private LayerMask groundLayerMask;
+    private float hoverRange = 7f;
 
+    [Header("WallCheck")]
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private float wallCheckDistance;
 
+    [Header("AnimationState")]
     [SerializeField] private animationStates currentState = animationStates.PlayerIdle;
+
+    
     public animationStates CurrentState
     {
         get
@@ -26,7 +47,7 @@ public class PlayerController : MonoBehaviour
         {
             if(currentState != value)
             {
-                anim.Play(value.ToString()); //replace with an event to an animation script or a generalized function
+                anim.Play(value.ToString());
                 currentState = value;
             }
         }
@@ -36,6 +57,9 @@ public class PlayerController : MonoBehaviour
     {
         PlayerIdle,
         PlayerRun,
+        PlayerAscent,
+        PlayerHover,
+        PlayerDescent
     }
 
     void Start()
@@ -53,20 +77,48 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         ApplyMovement();
+        CheckSurrounding();
+        CheckIfCanJump();
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        if (Mathf.Abs(movementInputDirection) < 0.1f && isGrounded)
+        {
+            CurrentState = animationStates.PlayerIdle;
+        }
+
+        if (Mathf.Abs(movementInputDirection) > 0.1f && isGrounded)
+        {
+            CurrentState = animationStates.PlayerRun;
+        }
+
+        if (rb.velocity.y > 0.1f)
+        {
+            CurrentState = animationStates.PlayerAscent;
+        }
+        else if (rb.velocity.y < hoverRange && rb.velocity.y > -hoverRange && !isGrounded)
+        {
+            CurrentState = animationStates.PlayerHover;
+        }
+        else if(rb.velocity.y < -hoverRange && !isGrounded)
+        {
+            CurrentState = animationStates.PlayerDescent;
+        }
+
     }
 
     private void CheckSurrounding()
     {
-        //isGrounded = Physics.OverlapSphere(groundCheck.position, groundCheckRadius, )
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayerMask);
+        isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, groundLayerMask);
     }
 
     private void CheckInput()
     {
         movementInputDirection = Input.GetAxisRaw("Horizontal");
-        if(Mathf.Abs(movementInputDirection) < 0.1f)
-        {
-            CurrentState = animationStates.PlayerIdle;
-        }
+        
         if (Input.GetButtonDown("Jump"))
         {
             Jump();
@@ -88,20 +140,43 @@ public class PlayerController : MonoBehaviour
     private void ApplyMovement()
     {
         rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
-        if(Mathf.Abs(movementInputDirection) > 0.1f)
-        {
-            CurrentState = animationStates.PlayerRun;
-        }
     }
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            currentJumpCount--;
+        }
+        
+    }
+
+    private void CheckIfCanJump()
+    {
+        if(isGrounded && rb.velocity.y <= 0)
+        {
+            currentJumpCount = jumpCount;
+        }
+        if(currentJumpCount <= 0)
+        {
+            canJump = false;
+        }
+        else
+        {
+            canJump = true;
+        }
     }
 
     private void Flip()
     {
         isFacingRight = !isFacingRight;
         transform.Rotate(0.0f, 180f, 0.0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
